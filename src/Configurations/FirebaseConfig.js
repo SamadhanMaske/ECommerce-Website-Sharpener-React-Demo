bfd948317bcaa0b1dd15c1410a2a2cf541c7b0f2
SamadhanMaske <samadhanmaske208@gmail.com>
2024-01-18 09:10:44 +0530

    Added firebase Configuration to starter project

---
+++ src/Configurations/FirebaseConfig.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1-tgpge0XtsfmJc0CT1LJi1o96SrLI7E",
+  authDomain: "ecommerce-website-sharpener.firebaseapp.com",
+  projectId: "ecommerce-website-sharpener",
+  storageBucket: "ecommerce-website-sharpener.appspot.com",
+  messagingSenderId: "695647329992",
+  appId: "1:695647329992:web:c40243fb753f621a444ffd"
+};
+
+const app = initializeApp(firebaseConfig);
